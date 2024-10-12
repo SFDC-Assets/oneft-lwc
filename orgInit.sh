7fafabccdac7708ca50d7e40cde40c76f5f52d58
Jonathan Zhou <34146693+zhou059@users.noreply.github.com>
2024-10-12 06:24:32 -0700

    Update orgInit.sh

--- orgInit.sh
+++ orgInit.sh
@@ -1,4 +1,4 @@
-sfdx force:org:create -f config/project-scratch-def.json -d 1 -s
-sfdx force:source:push
-sfdx force:apex:execute -f scripts/apex/hello.apex 
-sfdx force:org:open -p /lightning/o/Contact/list?filterName=All_Contacts
+sf demoutil org create scratch -f config/project-scratch-def.json -d 2 -s -p one -e ft.lwc
+sf project deploy start
+sf apex run -f scripts/apex/hello.apex 
+sf org open -p /lightning/o/Contact/list?filterName=All_Contacts
